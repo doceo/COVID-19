ba787b135cd69ca3265542a4bd6d5c687e9c0119
Diomede Mazzone <diomede.mazzone@gmail.com>
2021-03-10 23:05:52 +0100

    dati normalizzati

--- analisi.py
+++ analisi.py
@@ -11,8 +11,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 contagi = { }
-
+popolazione = { }
 dataSom = []
+
+'''
+
+Dal file delle regioni costruiamo un dict 
+con le popolazioni residenti
+
+'''
+
+with open('Tutte_le_regioni.csv', 'r') as file:
+    valori = csv.reader(file, delimiter=";")
+    header = next(valori)
+
+    for row in valori:
+
+        print("riga: ",row)
+        popolazione[row[1]] = row[4] 
+
+print(header)
+file.close()
+
+print(popolazione)
+
+'''
+a partire dagli opendata sui contagi si costruiscono i vettori
+da comprarare attraverso i grafici.
+
+
+'''
 
 # apertura file riga per riga organizzando il risultato in liste
 with open('dati-regioni/dpc-covid19-ita-regioni.csv', 'r') as file:
@@ -23,37 +51,41 @@ with open('dati-regioni/dpc-covid19-ita-regioni.csv', 'r') as file:
 
         data = datetime.fromisoformat(row[0])
         
-#        dataSom.append(data)
+        if row[3] in popolazione:
+            datoNorm = (int(row[12]))/int(popolazione[row[3]])*100000
 
         if not(row[3] in contagi):
             contagi[row[3]] = []
 
-        contagi[row[3]].append((data,int(row[12])))
+        contagi[row[3]].append((data,datoNorm))
 
 
 print(header)
 file.close()
 
+
+
 date = []
 val = []
 regioni = list(contagi.keys())
-regioni.remove("Campania")
 
 regMatrix = []
 
-for i in range(5):
+for i in range(7):
     regMatrix.append([])
 
 print(regMatrix)
 
 i = 0
 
-for row in range(5):
-    for col in range(4):
-        regMatrix[row].append(regioni[i])
+for row in range(7):
+    for col in range(3):
+        if(regioni[i]):
+            regMatrix[row].append(regioni[i])
         i = i+1
 print(regMatrix)
 
+regioni.remove("Campania")
 
 campVal = []
 campDate = []
@@ -64,29 +96,19 @@ for i in contagi["Campania"]:
     campDate.append(i[0])
     campVal.append(i[1])
 
-for i in range(5):
-    for j in range(4):
-        print(regMatrix[i][j])
-
-#print(campVal)
-
-#print(campDate)
-#print(campVal)
-#print(contagi["Campania"])
-
-
+print(regMatrix)
 
 nome = "contagi-"
 
-for j in range(5):
+for j in range(7):
 
-    fig, ax = plt.subplots(1, 4, figsize=(12,7))
+    fig, ax = plt.subplots(1, 3, figsize=(12,7))
 
     # generiamo una matrice di una riga per 4 colonne da inserire in Figure
-    left, bottom, width, height = 0.4, 0.4, 0.4, 0.4 
+    #left, bottom, width, height = 1, 1, 1, 1 
     #plt.xticks(rotation=70)
 
-    for i in range(4):
+    for i in range(3):
         reg = contagi[regMatrix[j][i]]
         
         for dati in reg:
@@ -100,11 +122,12 @@ for j in range(5):
         ax[i].legend()
         ax[i].grid(True)
         #ax[i].set_xlabel('date')
-        ax[i].set_ylabel('Contagi')
-        plt.setp( ax[i].xaxis.get_majorticklabels(), rotation=70 )   
+        #ax[i].set_ylabel('Contagi')
+        plt.setp( ax[i].xaxis.get_majorticklabels(), rotation=70 )
+        plt.setp( ax[i].yaxis.get_majorticklabels(), rotation=70 )   
 
-        print(len(val))
-        print(len(date))
+        #print(len(val))
+        #print(len(date))
         date.clear()
         val.clear()
 
